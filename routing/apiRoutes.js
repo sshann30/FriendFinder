6bc4ea34c0fb481b4bd3e469345fadfda16ad025
Steve Shannon <you@example.com>
2019-02-07 15:22:53 -0500

    10 more min with j after class

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -36,31 +36,45 @@ module.exports = function (app) {
 	})
 	app.post("/api/friends", function (req, res) {
 		// res.json(true);
-		var bestmatch = {name:"", photo:"", score:40}
+		var bestmatch = { name: "", photo: "", score: 40 }
+		// console.log(req.body)
 
 
-	// linked to  
-	// 	Name: $("#Name").val().trim(),
-    // Photo: $("#Photo").val().trim(),
-    // Scores: 
-
+		// linked to  
+		// 	Name: $("#Name").val().trim(),
+		// Photo: $("#Photo").val().trim(),
+		// Scores: 
+		var difference
 		var scoresOfUser = (req.body.Scores)
+		// console.log("===============================")
+		// console.log(scoresOfUser)
 		for (var i = 0; i < friends.length; i++) {
+			
+			if (bestmatch.score > difference) {
+				bestmatch.name = friends[i].name
+				bestmatch.photo = friends[i].photo
+				bestmatch.score = difference
+			}
+			difference = 0
 
 			// is amount inside of this lower than bestmatch.score
 			// if so then replace all 3 values inside of best match
-		}
+			for (var j = 0; j < scoresOfUser.length; j++) {
+				console.log(scoresOfUser)
+				// absolute value of difference between responce of friends and baseball players
+				difference += Math.abs(scoresOfUser [j] - friends[i].entered[j]) 
+				// console.log(difference)
+				// if (bestmatch.score > difference) {
+				// 	bestmatch.name = friends[i].name
+				// 	bestmatch.photo = friends[i].photo
+				// 	bestmatch.score = difference
+				// }
+			
 
-		 for (var j = 0; j < scoresOfUser.length; j++) {
-			// absolute value of difference between responce of friends and baseball players
-			// loop thru where 1 abolute value gets added to the sum of all previous values 
-			// check previous for loop
-		}
-
-
-
+				// loop thru where 1 abolute value gets added to the sum of all previous values 
+				// check previous for loop
+			}}
 
 		friends.push(req.body)
 	});
-	
 };
